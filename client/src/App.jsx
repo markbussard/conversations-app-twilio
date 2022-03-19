1b43609a9e102b4117acb4891f04bca682bfb0d6
markbussard <markbussard@outlook.com>
2022-03-19 07:08:24 -0700

    Add functionality for login / registration flow

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Login } from 'features/login';
+import { ConversationsApp } from 'features/conversations-app';
+import { LOADING } from 'constants';
+import { FriendlyNameInput } from 'features/friendly-name-input';
+import { getConversationsToken } from './store/slices/authSlice';
 
 export default function App() {
+  const dispatch = useDispatch();
+  const { token, status, user } = useSelector((state) => state.auth);
+
+  const { email = '' } = JSON.parse(localStorage.getItem('user')) || {};
+
+  useEffect(() => {
+    if (email.length) {
+      dispatch(getConversationsToken(email));
+    }
+  }, []);
+
+  if (!email || (!token && status !== LOADING)) {
+    return <Login />;
+  }
+
+  if (status === LOADING) {
+    return (
+      <div>Loading...</div>
+    );
+  }
+
+  if (user.name === undefined) {
+    return <FriendlyNameInput />;
+  }
+
   return (
-    <div>Conversations App</div>
+    <ConversationsApp />
   );
 }
